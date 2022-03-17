ff33fb5cbcf18384465f593f5e0c2f5292463672
ejnoleto <contato@emmanuelnoleto.com.br>
2022-03-17 09:31:12 -0300

    index table

--- resources/views/livewire/url/index.blade.php
+++ resources/views/livewire/url/index.blade.php
@@ -6,7 +6,7 @@
     <h4 class="card-title">
         <b> {{ $componentName }} | {{ $pageTitle }}</b>
     </h4>
-    <table class="table-warning">
+    <table class="table striped mt-1">
         <thead>
         <tr>
              <td>URL</td>
@@ -23,7 +23,32 @@
                     <td >{{ $u->acesso }}</td>
                     <td >{{ $u->status }}</td>
                     <td >{{ $u->imagem }}</td>
-                    <td> - </td>
+                    <td class="text-center">
+                        <a href="javascript:void(0)"
+                        wire:click="Edit({{ $u->id }})"
+                        class="btn btn-dark mtmobilie" title="Edit" data-toggle="modal" data-target="#theModal">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="feather feather-edit-3">
+                                    <path d="M12 20h9"></path>
+                                    <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+                            </svg>
+                        </a>
+
+                        <a href="javascript:void(0)"
+                        onclick="Confirm('{{ $u->id }}')"
+                        class="btn btn-dark mtmobilie" title="Delete">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="feather feather-trash-2">
+                                <polyline points="3 6 5 6 21 6"></polyline>
+                                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                                <line x1="10" y1="11" x2="10" y2="17"></line>
+                                <line x1="14" y1="11" x2="14" y2="17"></line>
+                            </svg>
+                        </a>
+
+                    </td>
                 </tr>                
             @endforeach
         </tbody>
